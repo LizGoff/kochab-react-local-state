9af755fe856a06bd4243aa53548fd72d89000b6f
Liz Goff <livingearthcreations@gmail.com>
2018-05-15 16:53:18 -0500

    strech mode done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,8 +9,11 @@ class App extends Component {
     super(props);
 
     this.state = {
-      user: 'Liz',
-      city: 'Vancouver',
+      user:
+        {
+          name: 'Liz',
+          city: 'Vancouver',
+        }
     };
     // this.handelChange = this.handelChange.bind(this); This is in older versions of React
   }
@@ -18,26 +21,26 @@ class App extends Component {
   handleChange = (event) => {
     console.log('event.target.value', event.target.value);
     // this.state.user = event.target.value;
-    this.setState({ user: event.target.value })
+    this.setState({ user: { name: event.target.value, city: this.state.user.city } })
   }
 
   cityChange = (event) => {
     console.log('event.target.value', event.target.value);
     // this.state.user = event.target.value;
-    this.setState({ city: event.target.value })
+    this.setState({ user: { name: this.state.user.name, city: event.target.value } })
   }
 
 
   stateButton = (event) => {
-    console.log('event.target.value', this.state);
+    console.log('event.target.value', this.state.user);
     // this.setState ({ state });
   }
 
   render() {
     return (
       <div>
-        <p> The current user is {this.state.user} </p>
-        <p> She would like to move to {this.state.city} </p>
+        <p> The current user is {this.state.user.name} </p>
+        <p> She would like to move to {this.state.user.city} </p>
 
         <input onChange={this.handleChange} placeholder="User Name" />
         <input onChange={this.cityChange} placeholder="City" />
